8a917acb6fee8abbf9631aae92aa8238a562a106
Lambda_School_Loaner_222 <lambda_school_loaner_222@Lambda-School-Loaner-222s-MacBook-Pro.local>
2020-07-14 15:37:22 -0700

    day 2 mvp

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,12 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+
+    def delete(self):
+        if self.prev:
+           self.prev.next = self.next
+        if self.next:
+           self.next.prev = self.prev
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,7 +33,25 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        # create a new node from the value passed in
+        new_node = ListNode(value)
+
+        # increment the length of the list by 1
+        self.length += 1
+
+        # if the head and tail are None
+        # set the head and tail to the new node created
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+
+        # else set the new node's next to the head
+        # set the head's prev to the new node
+        # set the head to be the new node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
         
     """
     Removes the List's current head node, making the
@@ -35,7 +59,15 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        # grab the current value from the head
+        value = self.head.value
+
+        # call delete on the list to remove the head
+        self.delete(self.head)
+
+        # return the value grabbed from the head
+        return value
+
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -43,7 +75,25 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        # create a new node from the value passed in
+        new_node = ListNode(value)
+
+        # increment the length of the list by 1
+        self.length += 1
+
+        # if the head and tail are None
+        # set the head and tail to the new node created
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+
+        # else set the prev node on the new node to the tail
+        # set the next on the tail to the new node
+        # set the tail to be the new node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
             
     """
     Removes the List's current tail node, making the 
@@ -51,32 +101,103 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        # grab the current value from the tail
+        value = self.tail.value
+
+        # call delete on the list to remove the tail
+        self.delete(self.tail)
+
+        # return the value grabbed from the tail
+        return value
             
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        # grab the value from the node passed in
+        value = node.value
+
+        # delete the node passed in from the list
+        self.delete(node)
+
+        # add node to the head of the list
+        self.add_to_head(value)
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        # grab the value from the node passed in
+        value = node.value
+
+        # delete the node passed in from the list
+        self.delete(node)
+
+        # add node to the end of the list
+        self.add_to_tail(value)
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+        # set the length of the list to minus 1
+        self.length -= 1
+
+        # if there is no head or tail, return out of the function
+        if not self.head and not self.tail:
+            return
+
+        # if the head and tail are the same, we only have one node
+        # remove it's references and it will be deleted.
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+
+        # if the list's head is the same as the node passed in
+        # set the head to the next node and remove references to the head
+        elif self.head is node:
+            self.head = node.next
+            node.delete()
+
+        # if the list's tail is equal to the node passed in
+        # set the tail equal to the previous node and remove references to the tail
+        elif self.tail is node:
+            self.tail = node.prev
+            node.delete()
+
+        # else the node is somewhere in the middle, remove references to the node passed in
+        else:
+            node.delete()
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        # if we don't have a head, return None
+        if not self.head:
+            return None
+
+        # set new variable max_value equal to the current head value
+        current = self.head
+        max_value = current.value
+
+        # while head is not None
+        while current:
+            # if the current value is greater than the current max_value
+            # replace max_value with the current node's value
+            if current.value > max_value:
+                max_value = current.value
+
+            # move the current node to the next in the list and re-compare
+            current = current.next
+
+        # by this time, we have the maximum value in the list so return it
+        return max_value
